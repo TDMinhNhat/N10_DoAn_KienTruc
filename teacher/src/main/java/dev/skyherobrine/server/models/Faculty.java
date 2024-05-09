273d0eeb224768014bf954b6205fca51abed1a32
TDMinhNhat <tdminhnhat13092003@gmail.com>
2024-05-10 00:33:28 +0700

    Create entities

---
+++ teacher/src/main/java/dev/skyherobrine/server/models/Faculty.java
@@ -0,0 +1,32 @@
+package dev.skyherobrine.server.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity @Table(name = "Faculty")
+@Getter @Setter @NoArgsConstructor
+public class Faculty {
+    @Id @Column(name = "FacultyID", nullable = false)
+    private String facultyID;
+    @Column(name = "FacultyName", nullable = false)
+    private String facultyName;
+    @ManyToOne @JoinColumn(name = "DepartmentID", nullable = false)
+    private Department department;
+    @Column(name = "Description", length = 100)
+    private String description;
+
+    public Faculty(String facultyID, String facultyName, Department department) {
+        this.facultyID = facultyID;
+        this.facultyName = facultyName;
+        this.department = department;
+    }
+
+    public Faculty(String facultyID, String facultyName, Department department, String description) {
+        this.facultyID = facultyID;
+        this.facultyName = facultyName;
+        this.department = department;
+        this.description = description;
+    }
+}

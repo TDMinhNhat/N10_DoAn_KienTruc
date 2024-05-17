1ad41a19ea93743bd87c31ad79865bd21b3d8dda
TranQuangHien <tqhien27102002@gmail.com>
2024-05-18 04:34:10 +0700

    fix

---
+++ src/services/student.service.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:7000/api';
+
+class StudentService {
+    async getPersonalInfo(id) {
+        const response = await axios.get(API_URL + '/student-info/get-personal-info/' + id)
+        return response;
+    }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default new StudentService();
